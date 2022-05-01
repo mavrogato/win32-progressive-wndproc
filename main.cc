923650e5e2cf686df95c710249e8bef0557af73d
Nakashima, Terumi <nakashima.terumi@gmail.com>
2022-05-02 01:11:05 +0900

    Coroutine allocated in the class data

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <complex>
+#include <numbers>
 #include <coroutine>
 
 #include <windows.h>
@@ -58,30 +59,51 @@ private:
 int main() {
     thread_local std::tuple<HWND, UINT, WPARAM, LPARAM> params;
     thread_local auto& [hwnd, message, wParam, lParam] = params;
-    auto command = []() noexcept -> progressive<bool> {
+    auto segment = []() noexcept -> progressive<bool> {
         for (;;) {
+            int i = 0;
             while (message != WM_LBUTTONDOWN) co_yield false;
+            SetTimer(hwnd, 1, 16, nullptr);
             SetCapture(hwnd);
-            auto bgn = std::complex(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+            auto pt0 = std::complex<int>(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
             co_yield true;
-            while (message != WM_LBUTTONUP) co_yield false;
-            auto end = std::complex(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
-            std::cout << bgn << '-' << end << std::endl;
-            HDC hdc = GetDC(hwnd);
-            MoveToEx(hdc, bgn.real(), bgn.imag(), nullptr);
-            LineTo(hdc, end.real(), end.imag());
-            ReleaseDC(hwnd, hdc);
+            while (message != WM_LBUTTONUP) {
+                if (message == WM_TIMER) {
+                    std::complex<int> pt1;
+                    GetCursorPos(reinterpret_cast<POINT*>(&pt1));
+                    ScreenToClient(hwnd, reinterpret_cast<POINT*>(&pt1));
+                    HDC hdc = GetDC(hwnd);
+                    for (int j = 0; j < std::abs(pt1-pt0)/32; ++j) {
+                        double d = std::fmod(std::numbers::phi * (++i), 1.0);
+                        auto pt = std::complex{
+                            static_cast<int>((1-d) * pt0.real() + d * pt1.real()),
+                            static_cast<int>((1-d) * pt0.imag() + d * pt1.imag()),
+                        };
+                        SetPixel(hdc, pt.real(), pt.imag(), 0xffffff);
+                    }
+                    ReleaseDC(hwnd, hdc);
+                    co_yield true;
+                    continue;
+                }
+                else {
+                    co_yield false;
+                    continue;
+                }
+            }
+            auto pt1 = std::complex<int>(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+            std::cout << pt0 << '-' << pt1 << std::endl;
             ReleaseCapture();
+            KillTimer(hwnd, 1);
             co_yield true;
         }
     }();
-    auto proc = [&command]() noexcept -> progressive<LRESULT> {
+    auto proc = [&segment]() noexcept -> progressive<LRESULT> {
         for (;;) {
             if (message == WM_DESTROY) {
                 break;
             }
             else {
-                if (command.result()) {
+                if (segment.result()) {
                     co_yield 0;
                 }
                 else {
@@ -121,7 +143,7 @@ int main() {
         GetModuleHandle(nullptr),
         LoadIcon(nullptr, IDI_APPLICATION),
         LoadCursor(nullptr, IDC_CROSS),
-        static_cast<HBRUSH>(GetStockObject(LTGRAY_BRUSH)),
+        static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)),
         nullptr,
         TEXT("066a831d-8430-4ed5-8c13-6d4abc8b1c5a"),
     };
